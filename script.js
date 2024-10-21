9063c2ecd4fc654ed694b81b81db0daaa36db43c
Joshua Miguel Rodelas <jrodelas_230000001203@uic.edu.ph>
2024-10-21 09:06:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+const toggleIcon = document.getElementById("icons");
+  
+  toggleIcon.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+    if(document.body.classList.contains("dark-mode")){
+      icons.src = "assets/sun.png";
+    }else{
+      icons.src = "assets/moon.png";
+    }
+  });
+
+  const darkModeToggle = document.getElementById('icons1');
+darkModeToggle.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode')
+  if(document.body.classList.contains("dark-mode")){
+      icons1.src = "assets/sun.png";
+    }else{
+      icons1.src = "assets/moon.png";
+    }
+  });
